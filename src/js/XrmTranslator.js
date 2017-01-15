1b41642b10ed1435ffef32e5ba3687f43a3fb9a5
Florian Krönert <Florian.Kroenert@gmx.de>
2017-01-15 14:38:01 +0100

    Further progress in implementing auto translate

--- src/js/XrmTranslator.js
+++ src/js/XrmTranslator.js
@@ -28,8 +28,28 @@
     var entityMetadata = {};
     var attributeMetadata = [];
     var availableLanguages = [];
+
+    var translationApiUrl = "https://glosbe.com/gapi/translate?from=[sourceLanguage]&dest=[destLanguage]&format=json&phrase=[phrase]&pretty=true&tm=false&callback=?";
     
     var currentEntity = null;
+    
+    function BuildTranslationUrl (fromLanguage, destLanguage, phrase) {
+        return translationApiUrl
+            .replace("?from=[sourceLanguage]", "?from=" + fromLanguage)
+            .replace("&dest=[destLanguage]", "&dest=" + destLanguage)
+            .replace("&phrase=[phrase]", "&phrase=" + phrase);
+    }
+    
+    function GetTranslation(fromLanguage, destLanguage, phrase) {
+        $.support.cors = true;
+        
+        return Promise.resolve($.ajax({
+            url: BuildTranslationUrl(fromLanguage, destLanguage, phrase),
+            type: "GET",
+            crossDomain: true,
+            dataType: "json"
+        }));
+    }
     
     function SchemaNameComparer (e1, e2) {
         if (e1.SchemaName < e2.SchemaName) {
@@ -206,6 +226,103 @@
                 alert(error);
             });
     }
+    
+    function ProposeTranslations(fromLcid, destLcid) {
+        LockGrid("Translating...");
+        
+        var records = GetGrid().records;
+        var translationRequests = [];
+        
+        for (var i = 0; i < records.length; i++) {
+            var record = records[i];
+            
+            if (record[destLcid] || (record.w2ui && record.w2ui.changes && record.w2ui.changes[destLcid])) {
+                continue;
+            }
+            
+            translationRequests.push(GetTranslation("deu", "eng", record[fromLcid]));
+        }
+        
+        Promise.all(translationRequests)
+            .then(function (responses) {
+                debugger;
+                UnlockGrid();
+            })
+            .catch(function(error) {
+                alert(error);
+            });
+    }
+    
+    function ShowTranslationPrompt () {
+        if (!w2ui.foo) {
+            var languageLcids = [];
+            
+            for (var i = 0; i < availableLanguages.length; i++) {
+                languageLcids.push(availableLanguages[i].toString());
+            }
+            
+            $().w2form({
+                name: 'translationPrompt',
+                style: 'border: 0px; background-color: transparent;',
+                formHTML: 
+                    '<div class="w2ui-page page-0">'+
+                    '    <div class="w2ui-field">'+
+                    '        <label>Source Lcid:</label>'+
+                    '        <div>'+
+                    '           <input name="sourceLcid" type="list"/>'+
+                    '        </div>'+
+                    '    </div>'+
+                    '    <div class="w2ui-field">'+
+                    '        <label>Target Lcid:</label>'+
+                    '        <div>'+
+                    '            <input name="targetLcid" type="list"/>'+
+                    '        </div>'+
+                    '    </div>'+
+                    '</div>'+
+                    '<div class="w2ui-buttons">'+
+                    '    <button class="w2ui-btn" name="cancel">Cancel</button>'+
+                    '    <button class="w2ui-btn" name="ok">Ok</button>'+
+                    '</div>',
+                fields: [
+                    { field: 'targetLcid', type: 'list', required: true, options: { items: languageLcids } },
+                    { field: 'sourceLcid', type: 'list', required: true, options: { items: languageLcids } }
+                ],
+                actions: {
+                    "ok": function () { 
+                        this.validate(); 
+                        
+                        ProposeTranslations(this.record.sourceLcid.id, this.record.targetLcid.id);
+                        this.close();
+                    },
+                    "cancel": function () {
+                        this.close(); 
+                    }
+                }
+            });
+        }
+        
+        $().w2popup('open', {
+            title   : 'Choose tranlations source and destination',
+            body    : '<div id="form" style="width: 100%; height: 100%;"></div>',
+            style   : 'padding: 15px 0px 0px 0px',
+            width   : 500,
+            height  : 300, 
+            showMax : true,
+            onToggle: function (event) {
+                $(w2ui.foo.box).hide();
+                event.onComplete = function () {
+                    $(w2ui.foo.box).show();
+                    w2ui.foo.resize();
+                }
+            },
+            onOpen: function (event) {
+                event.onComplete = function () {
+                    // specifying an onOpen handler instead is equivalent to specifying an onBeforeOpen handler, which would make this code execute too early and hence not deliver.
+                    $('#w2ui-popup #form').w2render('translationPrompt');
+                }
+            }
+        });
+    }
           
     function InitializeGrid (entities) {        
         $('#grid').w2grid({ 
@@ -215,6 +332,7 @@
                 footer: true,
                 toolbarSave: true
             },
+            multiSearch: true,
             columns: [                
                 { field: 'recid', caption: 'Metadata ID', size: '50px', sortable: true, resizable: true, hidden: true },
                 { field: 'schemaName', caption: 'Schema Name', size: '200px', sortable: true, resizable: true }
@@ -224,6 +342,7 @@
             },
             toolbar: {
                 items: [
+                    { type: 'button', id: 'autoTranslate', text: 'Auto Translate' },
                     { type: 'menu-radio', id: 'entitySelect', icon: 'fa-star',
                         text: function (item) {
                             var text = item.selected;
@@ -242,7 +361,10 @@
                 onClick: function(event) {
                     var target = event.target;
                     
-                    if (target.indexOf(":") !== -1) {
+                    if (target === "autoTranslate") {
+                        ShowTranslationPrompt();
+                    }
+                    else if (target.indexOf("entitySelect:") !== -1) {
                         var entity = target.substring(target.indexOf(":") + 1);
                         
                         currentEntity = entity;
