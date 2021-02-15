8e9aeead07e68a00ba1522dd40c8b1ef27209ca5
Florian Krönert <Florian.Kroenert@orbis.de>
2021-02-15 08:08:40 +0100

    Better filtering of attributes that can not be localized

--- src/js/Translator/AttributeHandler.js
+++ src/js/Translator/AttributeHandler.js
@@ -81,7 +81,24 @@
         grid.clear();
 
         var records = [];
-        var excludedColumnNumbers = [];
+
+        var excludedColumnNumbers = XrmTranslator.metadata.reduce(function(all, attribute) {
+            // If attribute has a formula definition, it is a rollup field.
+            // These always have directly following (in terms of column number) a state and a date field.
+            // These cause CRM exceptions when being translated, so we need to skip these
+            if (attribute.FormulaDefinition) {
+                if (attribute.AttributeType === "Money") {
+                    /// Skip _Base, _Date, _State
+                    all.push(attribute.ColumnNumber + 1, attribute.ColumnNumber + 2, attribute.ColumnNumber + 3);
+                }
+                else {
+                    // Skip _Date, _State
+                    all.push(attribute.ColumnNumber + 1, attribute.ColumnNumber + 2);
+                }
+            }
+
+            return all;
+        }, []);
 
         for (var i = 0; i < XrmTranslator.metadata.length; i++) {
             var attribute = XrmTranslator.metadata[i];
@@ -90,11 +107,9 @@
                 continue;
             }
 
-            // If attribute has a formula definition, it is a rollup field.
-            // These always have directly following (in terms of column number) a state and a date field.
-            // These cause CRM exceptions when being translated, so we need to skip these
-            if (attribute.FormulaDefinition) {
-                excludedColumnNumbers.push(attribute.ColumnNumber + 1, attribute.ColumnNumber + 2);
+            // Some attributes such as versionnumber can not be renamed / translated
+            if (attribute.IsRenameable && !attribute.IsRenameable.Value) {
+                continue;
             }
 
             var displayNames = attribute[XrmTranslator.GetComponent()].LocalizedLabels;
